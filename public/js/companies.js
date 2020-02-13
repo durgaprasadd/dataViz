1494166968d5decb3ea11c4acd8403583d0c166c
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2020-02-13 19:46:51 +0530

    Added transitition

--- public/js/companies.js
+++ public/js/companies.js
@@ -1,26 +1,16 @@
-const showData = (companies) => {
-    const toLine = company => `<strong>${company.Name}</strong> <i>${company.CMP}</i>`;
+const chartSize = { width: 800, height: 600 };
+const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+const width = chartSize.width - margin.left - margin.right;
+const height = chartSize.height - margin.top - margin.bottom;
+
+const showData = (companies, fieldName) => {
+    const toLine = company => `<strong>${company.Name}</strong> <i>${company[fieldName]}</i>`;
     document.querySelector('#chart-data').innerHTML = companies.map(toLine).join('<hr/>');
 }
 const drawChart = (companies) => {
-    const chartSize = {
-        width: 800,
-        height: 600
-    };
-
-    const margin = {
-        left: 100,
-        right: 10,
-        top: 10,
-        bottom: 150
-    };
-
-    const width = chartSize.width - margin.left - margin.right;
-    const height = chartSize.height - margin.top - margin.bottom;
 
     const colorScheme = d3.scaleOrdinal(d3.schemeCategory10)
-    const svg = d3.select('#chart-area')
-        .append('svg')
+    const svg = d3.select('#chart-area svg')
         .attr('height', chartSize.height)
         .attr('width', chartSize.width);
 
@@ -50,7 +40,7 @@ const drawChart = (companies) => {
         .range([0, width])
         .padding(0.3);
 
-    const y_axis = d3.axisLeft(y).ticks(5);
+    const y_axis = d3.axisLeft(y).ticks(10);
     const x_axis = d3.axisBottom(x);
 
     g.append('g').attr('class', 'y-axis').call(y_axis);
@@ -72,6 +62,50 @@ const drawChart = (companies) => {
         .attr('fill', company => colorScheme(company.Name));
 }
 
+const percentageFormat = d => `${d}%`
+const kCroreFormat = d => `${d / 1000}k Cr ₹`
+const formats = {
+    MarketCap: kCroreFormat,
+    QSales: kCroreFormat,
+    QNetProfit: kCroreFormat,
+    DivYld: percentageFormat,
+    ROCE: percentageFormat
+}
+
+const updateCompanies = (companies, fieldName) => {
+    const svg = d3.select("#chart-area svg");
+    svg.select('.y.axis-label').text(fieldName);
+
+    const y = d3.scaleLinear()
+        .domain([0, _.get(_.maxBy(companies, fieldName), fieldName, 0)])
+        .range([height, 0]);
+
+    const x = d3.scaleBand()
+        .domain(_.map(companies, 'Name'))
+        .range([0, width])
+        .padding(0.3);
+
+    const x_axis = d3.axisBottom(x);
+    const y_axis = d3.axisLeft(y).ticks(10).tickFormat(formats[fieldName]);
+    svg.select('.y-axis').call(y_axis);
+    svg.select('.x-axis').call(x_axis);
+
+    svg.selectAll('rect')
+        .data(companies, c => c.Name)
+        .exit()
+        .remove()
+
+    svg.selectAll('rect')
+        .data(companies, c => c.Name)
+        .transition()
+        .duration(1000)
+        .ease(d3.easeLinear)
+        .attr('y', c => y(c[fieldName]))
+        .attr('x', c => x(c.Name))
+        .attr('width', x.bandwidth)
+        .attr('height', c => y(0) - y(c[fieldName]))
+}
+
 const parseCompanies = (company) => {
     _.forEach(company, (value, key) => {
         if (!isNaN(value)) {
@@ -82,12 +116,18 @@ const parseCompanies = (company) => {
 }
 
 const drawCompanies = (companies) => {
-    showData(companies);
+    showData(companies, 'CMP');
     drawChart(companies);
 }
 
 const main = () => {
-    d3.csv('data/companies.csv', parseCompanies).then(drawCompanies);
+    d3.csv('data/companies.csv', parseCompanies).then((companies) => {
+        drawCompanies(companies);
+        const fields = "CMP,PE,MarketCap,DivYld,QNetProfit,QSales,ROCE".split(',');
+        let step = 1;
+        setInterval(() => { let field = fields[step++ % fields.length]; updateCompanies(companies, field); showData(companies, field) }, 1500)
+        setInterval(() => companies.shift(), 5000)
+    });
 }
 
 window.onload = main;
