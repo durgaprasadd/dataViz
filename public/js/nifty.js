77f949d169d7494154cad43788c1ab8a45f49560
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2020-02-19 10:33:02 +0530

    Added line of sma of period 100 days
    Added the function of analyseStrategy of sma of different periods

--- public/js/nifty.js
+++ public/js/nifty.js
@@ -1,4 +1,4 @@
-const chartSize = { width: 800, height: 600 };
+const chartSize = { width: 1040, height: 800 };
 const margin = { left: 100, right: 10, top: 10, bottom: 150 };
 const width = chartSize.width - margin.left - margin.right;
 const height = chartSize.height - margin.top - margin.bottom;
@@ -82,22 +82,74 @@ const updateQuotes = (quotes, fieldName1, fieldName2) => {
     const g = d3.select('.prices')
 
     const line = d3.line().x(q => x(new Date(q[fieldName1]))).y(q => y(q[fieldName2]))
+    const smaLine = d3.line().x(q => x(new Date(q[fieldName1]))).y(q => y(q.sma))
 
     g.append("path")
         .attr("class", "close")
         .attr("d", line(quotes))
+
+    g.append("path")
+        .attr("class", "sma")
+        .attr("d", smaLine(quotes.slice(99)))
 }
 
 const parseQuotes = (quote) => {
     _.forEach(quote, (value, key) => {
-        if (!isNaN(value)) {
+        if (key == 'Date') {
+            quote[key] = new window.Date(value)
+        }
+        else if (!isNaN(value)) {
             quote[key] = + value
         }
     })
     return quote;
 }
 
+const applyStrategy = (quotes, period) => {
+    let boughtTimes = 0;
+    let soldTimes = 0;
+    let investment = 100;
+    let stocks = 0;
+    let bought = false;
+    for(let i = period-1;i< quotes.length;i++){
+        let {Close,sma} = quotes[i];
+        if(!bought && Close>sma){
+            bought = true;
+            boughtTimes++;
+            stocks = investment/Close;
+        }
+        if(bought && (Close<sma || i == quotes.length-1)){
+            soldTimes++;
+            bought = false;
+            investment = stocks * Close;
+        }
+    }
+    return {boughtTimes,soldTimes,investment,bought,period}
+}
+
+const analyseData = (quotes, period) => {
+    for (let i = period; i < quotes.length + 1; i++) {
+        let sum = quotes.slice(i - period, i).reduce((a, b) => a + b.Close, 0);
+        const sma = _.round(sum / 100);
+        quotes[i - 1].sma = sma
+    }
+}
+
+const analyseStrategy = (quotes) => {
+    const analysis = []
+    for(let i = 1;i<quotes.length;i++){
+        let duplicate = quotes.slice();
+        analyseData(duplicate,i);
+        analysis.push(applyStrategy(duplicate,i));
+    }
+    console.log(_.maxBy(analysis,"investment"))
+}
+
 const startVisualization = (quotes) => {
+    let duplicate = quotes.slice();
+    analyseStrategy(duplicate);
+    analyseData(quotes, 100);
+    console.log(applyStrategy(quotes,100));
     drawChart();
     updateQuotes(quotes, "Date", "Close");
 }
