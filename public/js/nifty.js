1989f6e15481cf6eb424ed9e0672b00d22456c19
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2020-02-20 15:36:50 +0530

    Added slide bar functionality

--- public/js/nifty.js
+++ public/js/nifty.js
@@ -1,5 +1,5 @@
-const chartSize = { width: 1040, height: 800 };
-const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+const chartSize = { width: 1400, height: 650 };
+const margin = { left: 100, right: 10, top: 10, bottom: 80 };
 const width = chartSize.width - margin.left - margin.right;
 const height = chartSize.height - margin.top - margin.bottom;
 const colorScheme = d3.scaleOrdinal(d3.schemeCategory10)
@@ -21,7 +21,7 @@ const drawChart = () => {
     g.append("text")
         .attr('class', "x axis-label")
         .attr('x', width / 2)
-        .attr('y', height + 140)
+        .attr('y', height + margin.bottom - 10)
         .text("Time");
 
     g.append("text")
@@ -55,23 +55,65 @@ const formats = {
     ROCE: percentageFormat
 }
 
-const updateQuotes = (quotes, fieldName1, fieldName2) => {
-    showData(quotes, fieldName1, fieldName2)
+const getLocaleDate = (TimeStamp) => new Date(TimeStamp).toLocaleDateString('en-GB');
+const getRange = (begin, end) => getLocaleDate(begin) + "  -  " + getLocaleDate(end);
+
+const getStartIndex = (quotes, begin) => {
+    for (i = 0; i < quotes.length; i++) {
+        if (quotes[i].Date.getTime() >= begin) {
+            return i;
+        }
+    }
+    return 0;
+}
+
+const getEndIndex = (quotes, end) => {
+    for (i = quotes.length - 1; i >= 0; i--) {
+        if (quotes[i].Date.getTime() <= end) {
+            return i;
+        }
+    }
+    return quotes.length - 1;
+}
+
+const getQuotesInRange = (quotes, begin, end) => {
+    const startIndex = getStartIndex(quotes, begin);
+    const endIndex = getEndIndex(quotes, end);
+    console.log(startIndex, endIndex)
+    return quotes.slice(startIndex, endIndex + 1);
+}
+
+const showSlider = (quotes) => {
+    const fqDate = _.first(quotes).Date;
+    const lqDate = _.last(quotes).Date;
+    console.log(fqDate.getTime())
+    var slider = createD3RangeSlider(fqDate.getTime(), lqDate.getTime(), "#slider-container");
+    slider.range(fqDate.getTime(), lqDate.getTime())
+    d3.select("#range-label").text(getRange(fqDate, lqDate));
+    slider.onChange(function ({ begin, end }) {
+        d3.select("#range-label").text(getRange(begin, end));
+        const selectedQuotes = getQuotesInRange(quotes, begin, end);
+        renderSelectedQuotes(selectedQuotes, "Date", "Close");
+    });
+
+}
+
+const renderSelectedQuotes = (quotes, fieldName1, fieldName2) => {
+    console.log(quotes);
     const svg = d3.select("#chart-area svg");
     svg.select('.y.axis-label').text(fieldName2);
-
-    const fq = _.first(quotes);
-    const lq = _.last(quotes);
-
+    const fqDate = _.first(quotes).Date;
+    const lqDate = _.last(quotes).Date;
     const maxClose = _.get(_.maxBy(quotes, fieldName2), fieldName2, 0)
     const minClose = _.get(_.minBy(quotes, fieldName2), fieldName2, 0)
-
+    const maxSma = _.get(_.maxBy(quotes, "sma"), "sma", 0);
+    const minSma = _.get(_.minBy(quotes, "sma"), "sma", 0);
     const y = d3.scaleLinear()
-        .domain([minClose, maxClose])
+        .domain([Math.min(minClose, minSma), Math.max(maxClose, maxSma)])
         .range([height, 0]);
 
     const x = d3.scaleTime()
-        .domain([new Date(fq[fieldName1]), new Date(lq[fieldName1])])
+        .domain([fqDate, lqDate])
         .range([0, width])
 
     const x_axis = d3.axisBottom(x);
@@ -81,16 +123,59 @@ const updateQuotes = (quotes, fieldName1, fieldName2) => {
     svg.select('.x-axis').call(x_axis);
     const g = d3.select('.prices')
 
-    const line = d3.line().x(q => x(new Date(q[fieldName1]))).y(q => y(q[fieldName2]))
-    const smaLine = d3.line().x(q => x(new Date(q[fieldName1]))).y(q => y(q.sma))
+    const line = d3.line().x(q => x(q[fieldName1])).y(q => y(q[fieldName2]))
+    const smaLine = d3.line().x(q => x(q[fieldName1])).y(q => y(q.sma))
+
+    g.select(".close")
+        .attr("d", line(quotes))
+    let startSma = 0;
+    _.some(quotes, (quote, index) => { return quote.sma && (startSma = index) })
+    console.log(startSma)
+    g.select(".sma")
+        .attr("d", smaLine(quotes.slice(startSma)))
+}
+
+
+
+
+
+
+const updateQuotes = (quotes, fieldName1, fieldName2) => {
+    // showData(quotes, fieldName1, fieldName2)
+    const svg = d3.select("#chart-area svg");
+    svg.select('.y.axis-label').text(fieldName2);
+    const fqDate = _.first(quotes).Date;
+    const lqDate = _.last(quotes).Date;
+    const maxClose = _.get(_.maxBy(quotes, fieldName2), fieldName2, 0)
+    const minClose = _.get(_.minBy(quotes, fieldName2), fieldName2, 0)
+
+    const y = d3.scaleLinear()
+        .domain([minClose, maxClose])
+        .range([height, 0]);
+
+    const x = d3.scaleTime()
+        .domain([fqDate, lqDate])
+        .range([0, width])
+
+    const x_axis = d3.axisBottom(x);
+    const y_axis = d3.axisLeft(y).ticks(10).tickFormat(formats[fieldName2]);
+
+    svg.select('.y-axis').call(y_axis);
+    svg.select('.x-axis').call(x_axis);
+    const g = d3.select('.prices')
+
+    const line = d3.line().x(q => x(q[fieldName1])).y(q => y(q[fieldName2]))
+    const smaLine = d3.line().x(q => x(q[fieldName1])).y(q => y(q.sma))
 
     g.append("path")
         .attr("class", "close")
         .attr("d", line(quotes))
 
+    let startSma = 0;
+    _.some(quotes, (quote, index) => { return quote.sma && (startSma = index) })
     g.append("path")
         .attr("class", "sma")
-        .attr("d", smaLine(quotes.slice(99)))
+        .attr("d", smaLine(quotes.slice(startSma)))
 }
 
 const parseQuotes = (quote) => {
@@ -111,20 +196,20 @@ const applyStrategy = (quotes, period) => {
     let investment = 100;
     let stocks = 0;
     let bought = false;
-    for(let i = period-1;i< quotes.length;i++){
-        let {Close,sma} = quotes[i];
-        if(!bought && Close>sma){
+    for (let i = period - 1; i < quotes.length; i++) {
+        let { Close, sma } = quotes[i];
+        if (!bought && Close > sma) {
             bought = true;
             boughtTimes++;
-            stocks = investment/Close;
+            stocks = investment / Close;
         }
-        if(bought && (Close<sma || i == quotes.length-1)){
+        if (bought && (Close < sma || i == quotes.length - 1)) {
             soldTimes++;
             bought = false;
             investment = stocks * Close;
         }
     }
-    return {boughtTimes,soldTimes,investment,bought,period}
+    return { boughtTimes, soldTimes, investment, bought, period }
 }
 
 const analyseData = (quotes, period) => {
@@ -137,21 +222,23 @@ const analyseData = (quotes, period) => {
 
 const analyseStrategy = (quotes) => {
     const analysis = []
-    for(let i = 1;i<quotes.length;i++){
+    for (let i = 1; i < quotes.length; i++) {
         let duplicate = quotes.slice();
-        analyseData(duplicate,i);
-        analysis.push(applyStrategy(duplicate,i));
+        analyseData(duplicate, i);
+        analysis.push(applyStrategy(duplicate, i));
     }
-    console.log(_.maxBy(analysis,"investment"))
+    console.log(_.maxBy(analysis, "investment"))
 }
 
 const startVisualization = (quotes) => {
-    let duplicate = quotes.slice();
-    analyseStrategy(duplicate);
+    // let duplicate = quotes.slice();
+    // analyseStrategy(duplicate);
     analyseData(quotes, 100);
-    console.log(applyStrategy(quotes,100));
+    console.log(applyStrategy(quotes, 100));
     drawChart();
     updateQuotes(quotes, "Date", "Close");
+    showSlider(quotes);
+
 }
 
 const main = () => {
